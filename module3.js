6e741ea44ea61f1b496ac4d1362499be6ba6549d
SaiPranathi5 <saipranathi13@gmail.com>
2020-05-04 09:32:28 +0530

    assig 4

---
+++ module3.js
@@ -0,0 +1,9 @@
+var x=5;
+(funtion(window){
+	var helloSpeaker.name={};
+	var speakWord="Hello"
+	helloSpeaker.name=funtion speak(name){
+	console.log(speakWord + " "+name);
+	}
+	window.helloSpeaker= helloSpeaker;
+})(window);
